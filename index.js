a7746ee705ddd48c6a7eeacd6520a325290617e2
your_username <hemanthchirikonda@gmail.com>
2020-09-22 16:57:10 +0530

    url shortern added

--- index.js
+++ index.js
@@ -3,10 +3,38 @@ const app = express();
 const bodyParser= require('body-parser');
 const mongodb= require('mongodb');
 let bcrypt= require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const nodemailer = require("nodemailer");
+const shortId= require('shortid');
 const mongodbClint= mongodb.MongoClient;
 const url = 'mongodb+srv://hemanth:OHoCYn9ztyvAKrdH@cluster0.v7ugo.mongodb.net?retryWrites=true&w=majority';
 const cors= require('cors');
-const { response } = require('express');
+//const { response } = require('express');
+
+app.use(express.urlencoded({extended:false}));
+
+
+let authenticate= function(req,res,next){
+  //check if token present
+  if(req.headers.authorization){
+// check if token valid
+let verifyResult= jwt.verify(req.headers.authorization,"mnbvcxsertyuiolknb");
+// if valid then allow user
+if(verifyResult){
+    next();
+}else{
+    res.status(401).json({
+        "message":"invalid authorization"
+    })
+}
+  }else{
+      res.status(401).json({
+          "message":"no token present"
+      })
+  }
+  
+  
+}
 
 app.use(cors({
     origin:"http://127.0.0.1:5500"
@@ -35,10 +63,11 @@ if(email){
     req.body.Password= hash;
   //  console.log(hash);
     let user = await db.collection('users').insertOne({
-            "Address": req.body.Address,
+               "Email": req.body.Email,
+               "Dob":req.body.Dob,
+             "Address": req.body.Address,
             "Address2": req.body.Address2,
             "City": req.body.City,
-            "Email": req.body.Email,
             "Gridcheck": req.body.Gridcheck,
             "Password": req.body.Password,
             "State": req.body.State,
@@ -47,7 +76,6 @@ if(email){
     );
  
 }
-
 client.close();
 res.json({
     "message":"user created"
@@ -61,14 +89,18 @@ res.json({
 });
 
 app.post("/login",async (req,res)=>{
-    try {let client = await mongodbClint.connect(url);
+    try {
+        let client = await mongodbClint.connect(url);
         let db= client.db('trimurlapp');
         let user= await db.collection('users').findOne({'Email':req.body.Email});
         if(user){
           var result= await bcrypt.compare(req.body.Password,user.Password);
           if(result){
+              let token= jwt.sign({Email:user.Email},"mnbvcxsertyuiolknb");
+              //console.log(token);
               res.json({
-                  "message":"Allow"
+                  "message":"Allow",
+                  token
               });
           }else{
              res.json({
@@ -90,7 +122,132 @@ app.post("/login",async (req,res)=>{
         })
     }
 
+});
+
+
+app.get("/dashboard", authenticate, async (req,res)=>{
+    try {
+        let client = await mongodbClint.connect(url);
+        let db= client.db('trimurlapp');
+        let shorturl= await db.collection('shorturls').find().toArray();
+        client.close();
+        res.json(shorturl);
+    } catch (error) {
+        res.json(error)
+    }
+
+});
+
+app.post("/dashboard", authenticate, async (req,res)=>{
+    try {
+        let client = await mongodbClint.connect(url);
+        let db= client.db('trimurlapp');
+        let shorturl= await db.collection('shorturls').insertOne({
+            "full":req.body.full,
+              "short":shortId.generate(),
+              "clicks":0,
+              "user":req.body.Email
+        });
+        client.close();
+        res.json({
+            "message":"url created"
+        });
+    } catch (error) {
+        res.json(error)
+    }
+
 })
+
+app.get("/:code", async (req,res)=>{
+    //console.log(hello)
+    try {
+        let variable= req.params.code;
+        let client = await mongodbClint.connect(url);
+        let db= client.db('trimurlapp');
+        //console.log(shortUrl);
+        let shorturl= await db.collection('shorturls').findOne({short: variable});
+        console.log(shorturl);
+        if(shorturl=== null) return res.status(404);
+        shorturl.clicks++;
+        console.log(shorturl)
+        res.redirect(shorturl.full);
+        client.close();
+       res.json(shorturl)
+    } catch (error) {
+        console.log(error)
+        res.json(error)
+    }
+})
+
+
+
+
+app.post("/gencode", async (req,res)=>{
+ try {
+     let client = await mongodbClint.connect(url);
+    let db= client.db('trimurlapp');
+    let user= await db.collection('users').findOne({'Email':req.body.Email});
+    if(user){
+        let result =  (req.body.Dob === user.Dob);
+      if(result){
+        let code= Math.ceil(Math.random()*(999999-100000)+100000);
+        let salt= bcrypt.genSalt(10);
+        let hash= bcrypt.hash(code,salt);
+    await db.collection('users').findOneAndUpdate({"Email":req.body.Email},{$set:{"Password":hash}});
+      }else{
+        res.json({
+            "message":"invalid details"
+        })
+      }
+
+    }else{
+        res.json({
+            "message":"invalid User"
+        })
+    }
+    client.close();
+    res.json({
+        "message":"Code genareted"
+    })
+ } catch (error) {
+     res.json({
+         "message":error
+     })
+ }
+})
+app.post("/gencode", async (req,res)=>{
+ try {
+     let client = await mongodbClint.connect(url);
+    let db= client.db('trimurlapp');
+    let user= await db.collection('users').findOne({'Email':req.body.Email});
+    if(user){
+        let result =  (req.body.Dob === user.Dob);
+      if(result){
+        let code= Math.ceil(Math.random()*(999999-100000)+100000);
+        let salt= bcrypt.genSalt(10);
+        let hash= bcrypt.hash(code,salt);
+    await db.collection('users').findOneAndUpdate({"Email":req.body.Email},{$set:{"Password":hash}});
+      }else{
+        res.json({
+            "message":"invalid details"
+        })
+      }
+
+    }else{
+        res.json({
+            "message":"invalid User"
+        })
+    }
+    client.close();
+    res.json({
+        "message":"Code genareted"
+    })
+ } catch (error) {
+     res.json({
+         "message":error
+     })
+ }
+});
 
 
 
